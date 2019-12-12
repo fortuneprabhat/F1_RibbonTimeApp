c772868142367a5de4cecfc5a67b053eaec4d511
Prabhat Kumar <prabhat.kumar05@nagarro.com>
2019-12-12 17:06:58 +0530

    Ribbon time app

---
+++ src/main/java/com/ribbonTimeApp/F1_RibbonTimeApp/F1RibbonTimeAppApplication.java
@@ -0,0 +1,13 @@
+package com.ribbonTimeApp.F1_RibbonTimeApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1RibbonTimeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F1RibbonTimeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ribbonTimeApp/F1_RibbonTimeApp/F1RibbonTimeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ribbonTimeApp.F1_RibbonTimeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F1RibbonTimeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
